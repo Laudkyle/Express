42ea657d0a9b91c3820ba0ca678f4948f5700ea1
Laudkyle <kyleaby1@gmail.com>
2024-09-25 16:12:06 +0000

    Static setup

--- server.js
+++ server.js
@@ -2,13 +2,10 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','index.html'))
-})
 
-app.get('/about',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','about.html'))
-})
+// Setting up the static 
+app.use(express.static(path.join(__dirname,'public')))
+
 
 app.listen(4000,()=>{
     console.log("Server is running on port 4000")
