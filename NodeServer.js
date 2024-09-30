56dd957aec6376343e15cecf658ecc6ae24c6b8f
Laudkyle <kyleaby1@gmail.com>
2024-09-30 06:38:43 +0000

    By my self

---
+++ NodeServer.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const port = process.env.PORT || 5000
+
+const app  = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+let posts = [
+    { id: 1, title: "Post One" },
+    { id: 2, title: "Post Two" },
+    { id: 3, title: "Post Three" },
+    { id: 4, title: "Post Four" },
+  ];
+
+
+
+app.listen(port,()=>{
+    console.log(`Server Started on Port ${port}`)
+})
